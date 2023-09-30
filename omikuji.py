ce12d433cdfac9752610d2c057cabb44e9084851
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-09-30 17:26:12 +0900

    first commit

---
+++ omikuji.py
@@ -0,0 +1,11 @@
+import random
+
+# omikuji_list = ["凶", "吉", "大吉"]
+
+# print(f"今日の運勢は...{random.choice(omikuji_list)}")
+
+omikuji_list = ["凶", "吉", "大吉"]
+
+result = random.choice(omikuji_list)
+
+print(f"今日の運勢は...{result}")
